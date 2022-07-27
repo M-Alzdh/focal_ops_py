cf25d5f16e33633a67c67a61d7f6128abd3d9906
Jamil <Jamil.Dabbaghian@gmail.com>
2022-07-27 20:43:39 +0430

    created neighborfinding function, the operation can now be run manually
    function to be added later

--- focal_ops.py
+++ focal_ops.py
@@ -1,6 +1,7 @@
 from ctypes import resize
 import math as m
 import random as rnd
+from statistics import mean
 import numpy as np
 
 random_numbers = np.empty(16)
@@ -16,8 +17,46 @@ h_pad = np.zeros(shape=(1, 4))
 
 h_padded = np.vstack((h_pad, random_numbers, h_pad))
 
-print(h_padded)
+
 
 v_pad = np.zeros(shape = (6, 1))
 
 hv_padded = np.hstack((v_pad, h_padded, v_pad))
+
+"""
+indx = [1, 1]
+
+num_neighbor = 1
+
+left = max(0, indx[0]-num_neighbor)
+right = max(0, indx[0]+num_neighbor+1)
+
+bottom = max(0,indx[1]-num_neighbor)
+top = max(0,indx[1]+num_neighbor+1)
+
+selection = hv_padded[left:right, bottom:top]
+
+print(selection)
+
+print(np.mean(selection))
+print(selection[indx[0], indx[1]])
+
+"""
+
+new_array = np.empty(shape=(6, 6))
+
+for i in range(hv_padded.shape[0]):
+    for j in range(hv_padded.shape[1]):
+        indx = [i, j]
+        num_neighbor = 1
+        left = max(0, indx[0]-num_neighbor)
+        right = max(0, indx[0]+num_neighbor+1)
+
+        bottom = max(0,indx[1]-num_neighbor)
+        top = max(0,indx[1]+num_neighbor+1)
+        selection = hv_padded[left:right, bottom:top]
+        value = np.round(np.mean(selection), 2)
+        new_array[indx[0], indx[1]] = value
+
+print(hv_padded)
+print(new_array)
