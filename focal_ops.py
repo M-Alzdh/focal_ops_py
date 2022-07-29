da9174a03a071110d97f53a35c25c372a8d8479b
Jamil <Jamil.Dabbaghian@gmail.com>
2022-07-29 17:51:59 +0430

    cleaned up the code, removed the manual tests

--- focal_ops.py
+++ focal_ops.py
@@ -1,66 +1,4 @@
-from ctypes import resize
-import math as m
-import random as rnd
-from statistics import mean
 import numpy as np
-
-random_numbers = np.empty(16)
-
-for i in range(len(random_numbers)):
-    random_numbers[i] = rnd.randint(1, 10)
-
-random_numbers.shape = (4,4)
-
-h_pad = np.zeros(shape=(1, 4))
-
-#print(h_pad)
-
-h_padded = np.vstack((h_pad, random_numbers, h_pad))
-
-
-
-v_pad = np.zeros(shape = (6, 1))
-
-hv_padded = np.hstack((v_pad, h_padded, v_pad))
-
-"""
-indx = [1, 1]
-
-num_neighbor = 1
-
-left = max(0, indx[0]-num_neighbor)
-right = max(0, indx[0]+num_neighbor+1)
-
-bottom = max(0,indx[1]-num_neighbor)
-top = max(0,indx[1]+num_neighbor+1)
-
-selection = hv_padded[left:right, bottom:top]
-
-print(selection)
-
-print(np.mean(selection))
-print(selection[indx[0], indx[1]])
-
-"""
-
-new_array = np.empty(shape=(6, 6))
-
-for i in range(hv_padded.shape[0]):
-    for j in range(hv_padded.shape[1]):
-        indx = [i, j]
-        num_neighbor = 1
-        left = max(0, indx[0]-num_neighbor)
-        right = max(0, indx[0]+num_neighbor+1)
-
-        bottom = max(0,indx[1]-num_neighbor)
-        top = max(0,indx[1]+num_neighbor+1)
-        selection = hv_padded[left:right, bottom:top]
-        value = np.round(np.mean(selection), 2)
-        new_array[indx[0], indx[1]] = value
-
-print(hv_padded)
-print(new_array)
-
 
 def focal_ops(x, op_function = 'mean'):
     #performs focal operations
@@ -95,5 +33,10 @@ def focal_ops(x, op_function = 'mean'):
     return(new_array)
     
 
-focal_ops(hv_padded, op_function='max')
+random_array = np.array(np.random.random_integers(0, 100, size = (4,5)))
+
+print(random_array)
+
+focal_ops(random_array)
+
 
